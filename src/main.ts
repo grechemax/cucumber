e3140cd22662c602f3251c05832ceab0f105d00e
Maks Hrech <grechemax1@gmail.com>
2025-12-16 15:06:05 +0100

    Add main.ts

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { setDefaultTimeout, setWorldConstructor } from "@cucumber/cucumber";
+import { CustomWorld } from "./worlds/custom.world.js";
+
+setDefaultTimeout(999999);
+setWorldConstructor(CustomWorld);
